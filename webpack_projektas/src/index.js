956be45a88209ead045a04ee809543cacef0dacc
vygandas <vygandas.pliasas@gmail.com>
2018-06-03 15:17:18 +0300

    pvz

--- webpack_projektas/src/index.js
+++ webpack_projektas/src/index.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
 (function() {
-  axios.get('https://raw.githubusercontent.com/vygandas/javascript-pagrindai-labas-pasauli/master/Data/knygos.json#')
+  axios.get('https://raw.githubusercontent.com/vygandas/javascript-pagrindai-labas-pasauli/master/Data/knygos.json?dsfsdf=455')
     .then(function (response) {
       console.log(response);
-      let knygos = JSON.parse(response.data);
+      let knygos = response.data;
       console.log(knygos);
       console.log(knygos.length);
+
+      knygos.map(function(k) {
+        console.log('Pavadinimas', k.title);
+      });
     })
     .catch(function (error) {
       console.log(error);
